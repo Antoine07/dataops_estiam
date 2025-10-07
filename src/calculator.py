8fcf3594e11905dbf4262f2ad52094615166ad1a
Antoine07 <antoine.lucsko@wanadoo.fr>
2025-10-07 23:01:24 +0200

    f

--- src/calculator.py
+++ src/calculator.py
@@ -1,3 +1,7 @@
 def add(a: int, b: int) -> int:
     """Additionne deux entiers uniquement."""
     return a + b
+
+def subtract(a: int, b: int) -> int:
+    """Soustrait deux entiers uniquement."""
+    return a - b
